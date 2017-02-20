8948a088addc8295febc76595293b0ae0487f166
Danielos15 <danielos15@ru.is>
2017-02-20 21:47:08 +0000

    update styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('src/sass/index.scss')
+  return gulp.src('src/sass/styles.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('src/css'))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('src/'));
 });
 
 gulp.task('sass:watch', function () {
